2d219e444ba06247bc66ff801795b14c9eab7857
NgoAnhKhoa <anhkhoa.ngo2017@gmail.com>
2021-11-24 15:33:07 +0700

    delete user feature (Khoa Tran)

--- admin/user_list.php
+++ admin/user_list.php
@@ -17,7 +17,8 @@
                 <th>ID</th>
                 <th>User name</th>
                 <th>Email</th>
-                <th>State</th>
+                <th class="text-center">State</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
@@ -31,13 +32,12 @@
                 echo "<td>$name</td>";
                 echo "<td>$email</td>";
                 if($state) {
-                    // echo "<td><a href='lock_user.php?id=$id' style='color: red;'>Lock</a></td>";
-                    echo '<td><a href="lock_user.php?id=' . $id . '"><img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/258844251_6445222162219753_17915607980117620_n.png?_nc_cat=104&ccb=1-5&_nc_sid=aee45a&_nc_ohc=Y9ekoctgjiIAX8s0OY4&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=a4a4f722f899ead7c4759c4c797c8396&oe=61C246C1"></a></td>';
+                    echo '<td class="text-center"><a href="lock_user.php?id=' . $id . '"><i class="fa fa-unlock" style="color: green;"></i></a></td>';
                 }
                 else {
-                    // echo "<td><a href='unlock_user.php?id=$id' style='color: green;'>Unlock</a></td>";
-                    echo '<td><a href="unlock_user.php?id=' . $id . '"><img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/cp0/256662355_4552326314881717_4362225884319688671_n.png?_nc_cat=101&ccb=1-5&_nc_sid=aee45a&_nc_ohc=FGWBmzCE-IMAX9w_ciT&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=d968281da3c1812c829b42388282d01e&oe=61C12F1A"></a></td>';
+                    echo '<td><a href="unlock_user.php?id=' . $id . '"><i class="fa fa-lock" style="color: red;"></i></a></td>';
                 }
+                echo '<td><a style="color:red;text-decoration:none;" href="delete_user.php?id=' . $id . '">Delete</td>';
                 echo "</tr>";
             } ?>
         </tbody>
