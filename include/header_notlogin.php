3337acbb6f0aaa47773ad121be4c4ff9a391f5c9
NgoAnhKhoa <anhkhoa.ngo2017@gmail.com>
2021-11-28 22:39:08 +0700

    add SEO

--- include/header_notlogin.php
+++ include/header_notlogin.php
@@ -52,6 +52,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="BK Apple Store chuyên cung cấp các sản phẩm Apple. BK Apple Store luôn đảm bảo chất lượng sản phẩm chính hãng, khuyến mãi tốt">
+    <meta name="keywords" content="bk apple store, ipad, iphone, watch, macbook, mac, apple">
     <title>
         <?php
             echo $title;
